4aea180ea1f0ffebbb07df39f6d1ccff5d1d180a
Mauricio Arce <mauricio.arce.mat@gmail.com>
2018-02-23 20:51:04 -0400

    Add project files.

---
+++ AbsolemHeart/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace AbsolemHeart.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AbsolemHeart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AbsolemHeart.WebApiApplication" Language="C#" %>
